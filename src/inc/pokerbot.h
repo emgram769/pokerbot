b6689ea90f23ed066e6741d433effe64d46dd6ef
Bram <bwasti@gmail.com>
2014-10-11 13:18:44 -0400

    added functionality

--- src/inc/pokerbot.h
+++ src/inc/pokerbot.h
@@ -6,10 +6,17 @@
 #define NUM_SUITS   4
 #define NUM_RANKS   13
 
+#define NUM_BOARD_CARDS 5
+#define NUM_HELD_CARDS 2
+#define NUM_HAND_CARDS 5
+
 enum suit { DIAMOND, CLUB, HEART, SPADE };
-enum rank { ACE = 1, TWO, THREE, FOUR, FIVE,
+enum rank { UNKNOWN, ACE_LOW, TWO, THREE, FOUR, FIVE,
             SIX, SEVEN, EIGHT, NINE, TEN,
-            JACK, QUEEN, KING };
+            JACK, QUEEN, KING, ACE_HIGH };
+enum hand_rank { HIGH_CARD, PAIR, TWO_PAIR,
+                 TRIPLE, STRAIGHT, FLUSH, FULL_HOUSE,
+                 QUADRUPLE, STRAIGHT_FLUSH };
 
 /* Cards are defined as follows:
  *  suit s = card_num / NUM_RANKS;
@@ -18,9 +25,14 @@ enum rank { ACE = 1, TWO, THREE, FOUR, FIVE,
  */
 typedef unsigned int card;
 
+typedef struct _hand {
+    hand_rank type;                 /* The highest hand rank made. */
+    card cards[NUM_HAND_CARDS];     /* Cards in order or high card to low. */
+} hand;
+
 typedef struct _player {
     void *history;                  /* TODO: make this meaningful. */
-    card cards[2];                  /* Contains the cards of the player. */
+    card cards[NUM_HELD_CARDS];     /* Contains the cards of the player. */
     unsigned long long chips;       /* Current number of chips. */
     unsigned long long bet;         /* Most recent bet made by the player. */
     unsigned int pos;               /* Position of the player. */
@@ -30,7 +42,7 @@ typedef struct _game_state{
     unsigned long long pot;         /* Count of the chips in the pot. */
     unsigned long long curr_bet;    /* The current highest bet for the round. */
     unsigned int curr_pos;          /* Current player betting. */
-    card board[5];                  /* Contains the flop, turn and river. */
+    card board[NUM_BOARD_CARDS];    /* Contains the flop, turn and river. */
     player *hero;                   /* The bot's own player data. */
     player **players;               /* An array of players. */
 } game_state;
--- src/pokerbot.cpp
+++ src/pokerbot.cpp
@@ -5,6 +5,159 @@
 #include <time.h>
 #include <stdlib.h>
 
+/* Gets the suit and rank of a card and returns them through
+ * passed in references. Returns 0 on success and -1 on failure.*/
+int get_card(card c, suit * s, rank * r) {
+    /* Bounds check on the card. */
+    if (c > (NUM_SUITS * NUM_RANKS)) {
+        return -1;
+    }
+    
+    /* Rank is 0 if unkown, and 1-13 if known. */
+    *r = (rank)(c % (NUM_RANKS + 1));
+
+    /* Suit is always 0 - 3. */
+    *s = (suit)(c / NUM_SUITS);
+
+    return 0;
+}
+
+/* Checks if a card is known. */
+static inline bool is_known(card c) {
+    if (c > (NUM_SUITS * NUM_RANKS)) {
+        /* Should raise an error. */
+        return 0;
+    } else if (c % (NUM_RANKS + 1) == 0) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
+int compare_function(const void *a,const void *b) {
+    int *x = (int *) a;
+    int *y = (int *) b;
+    return *x - *y;
+}
+
+/* Sort the cards to make the best hand.  The indices are guaranteed
+ * to be include (calculated by the calling function). The number of
+ * indices is n. */
+static inline void sift_best_cards(card * cards, int * indices, int n) {
+    /* Sort the indices. */
+    qsort(indices, n, sizeof(int), compare_function);
+
+    for (int i = 0; i < n; ++i) {
+        /* Swap to the front. Indices must be sorted for this work. */
+        cards[i] = cards[indices[i]];
+    }
+
+    int * best_cards = (int*)malloc(sizeof(int) * (NUM_HAND_CARDS - n));
+    for (int i = n; i < NUM_BOARD_CARDS + NUM_HELD_CARDS; ++i) {
+    }
+}
+
+/* Returns if 7 cards can make a pair. */
+static inline bool is_pair(card * cards) {
+    /* The rank of the best pair. */
+    rank best_pair = UNKNOWN;
+    /* Indices of the best pair. */
+    int b1, b2;
+
+    /* Iterate to the second to last card, searching for a pair. */
+    for (int i = 0; i < NUM_BOARD_CARDS + NUM_HELD_CARDS - 1; ++i) {
+        int candidate_index = i;
+        rank r1; suit s1;
+        get_card(cards[i], &s1, &r1);
+        
+        /* Only better pairs matter. */
+        if (r1 < best_pair) continue;
+
+        for (int j = i + 1; j < NUM_BOARD_CARDS + NUM_HELD_CARDS; ++j) {
+            rank r2; suit s2;
+            get_card(cards[j], &s2, &r2);
+            if (r2 == r1) {
+                best_pair = r1;
+                b1 = i;
+                b2 = j;
+            }
+        }
+    }
+
+    return 0;
+}
+
+/* Returns if 7 cards can make a two pair. */
+static inline bool is_two_pair(card * cards) {
+    for (int i = 0; i < NUM_BOARD_CARDS + NUM_HELD_CARDS; ++i) {
+
+    }
+
+    return 0;
+}
+
+/* Returns if 7 cards can make three of a kind. */
+static inline bool is_triple(card * cards) {
+    for (int i = 0; i < NUM_BOARD_CARDS + NUM_HELD_CARDS; ++i) {
+
+    }
+    
+    return 0;
+}
+
+/* Returns if 7 cards can make a straight. */
+static inline bool is_straight(card * cards) {
+    for (int i = 0; i < NUM_BOARD_CARDS + NUM_HELD_CARDS; ++i) {
+
+    }
+    
+    return 0;
+}
+
+/* Returns if 7 cards can make a flush. */
+static inline bool is_flush(card * cards) {
+    for (int i = 0; i < NUM_BOARD_CARDS + NUM_HELD_CARDS; ++i) {
+
+    }
+    return 0;
+}
+
+/* Returns if 7 cards can make a full house. */
+static inline bool is_full_house(card * cards) {
+    for (int i = 0; i < NUM_BOARD_CARDS + NUM_HELD_CARDS; ++i) {
+
+    }
+    return 0;
+}
+
+/* Returns if 7 cards can make a four of a kind. */
+static inline bool is_quadruple(card * cards) {
+    for (int i = 0; i < NUM_BOARD_CARDS + NUM_HELD_CARDS; ++i) {
+
+    }
+    return 0;
+}
+
+/* Returns if 7 cards can make a straight flush. */
+static inline bool is_straight_flush(card * cards) {
+    for (int i = 0; i < NUM_BOARD_CARDS + NUM_HELD_CARDS; ++i) {
+
+    }
+    return 0;
+}
+
+void simulate_hands(game_state * game) {
+
+    for (int i = 0; i < NUM_BOARD_CARDS; ++i) {
+        if (is_known(game->board[i])) {
+        } else {
+
+        }
+    }
+
+    return;
+}
+
 unsigned long long bot_choice(game_state * game) {
     return rand();
 }
